a717f09db62d86451d9016b5d0167f7e01c542c2
uyjulian <uyjulian@gmail.com>
2021-11-27 10:27:57 -0600

    Fix pointer int casting

--- config.h
+++ config.h
@@ -1,15 +1,15 @@
 #pragma once
 
+#include <stdint.h>
+
 #ifndef __UNCONST
-#define __UNCONST(a)   ((void *)(unsigned long)(const void *)(a))
+#define __UNCONST(a)   ((void *)(uintptr_t)(const void *)(a))
 #endif /* __UNCONST */
 
 #ifndef USE_ARG
 #define USE_ARG(x)       /*LINTED*/(void)&(x)
 #endif /* USE_ARG */
 
-
-#include <stdint.h>
 
 typedef uint8_t u_char;
 
